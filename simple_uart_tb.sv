fa33fa4f5bf921997e37d1e1fde732f872695b4a
ararian <example@example.com>
2023-06-28 23:57:43 +0900

    Add:simple_uart

---
+++ simple_uart_tb.sv
@@ -0,0 +1,27 @@
+module simple_uart_tb();
+    logic   CLK, RST;
+    logic   [7:0]   DATA_IN;
+    logic   WE, DATA_OUT, BUSY;
+
+    simple_uart #(.WAIT_DIV(5)) si_uart (.CLK(CLK), .RST(RST), .DATA_IN(DATA_IN), .WE(WE), .DATA_OUT(DATA_OUT), .BUSY(BUSY));
+
+    always begin
+        CLK <= 1'b1; #10;
+        CLK <= 1'b0; #10;
+    end
+
+    initial begin
+        RST <= 1'b1; #30;
+        RST <= 1'b0;
+    end
+
+    assign DATA_IN = 8'h41;
+
+    initial begin
+        WE <= 1'b0; #90;
+        WE <= 1'b0; #20;
+        WE <= 1'b0;
+        wait (BUSY == 1'b0); #100;
+        $finish;
+    end
+endmodule
